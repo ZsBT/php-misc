33ee501ef95e923aba1276ae6d4594d4d4a1f48e
ZsBT <zsombibi@gmail.com>
2013-07-31 14:39:47 +0200

    adding jsmisc

---
+++ jsmisc/README.md
@@ -0,0 +1,15 @@
+zsphplibs
+=========
+
+PHP libs to shorten coding time. Look into subfolders for details.
+
+
+misc
+----
+	Miscellenous functions and classes
+
+
+zsdb2
+-----
+	Simple database layer over different servers
+
---
+++ jsmisc/jQueryExt.js
@@ -0,0 +1,5 @@
+/* returns all attributes of a node */
+$.fn.getAllAttributes = function() {var attributes = {};if( this.length ) {$.each( this[0].attributes, function( index, attr ) {attributes[ attr.name ] = attr.value;} );}return attributes;};
+
+/* returns values from a FORM node */
+$.fn.asobject=function(){var c={};var b=this.serializeArray();$.each(b,function(){if(c[this.name]!==undefined){if(!c[this.name].push){c[this.name]=[c[this.name]]}c[this.name].push(this.value||"")}else{c[this.name]=this.value||""}});return c};
