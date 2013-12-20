b7e4fcb5086d3b4c72998b3a18f991d6603c5b99
ZsBT <zsombibi@gmail.com>
2013-12-20 21:00:49 +0100

    disable/enable form elements

--- jsmisc/jQueryExt.js
+++ jsmisc/jQueryExt.js
@@ -1,3 +1,8 @@
+/* Zsombor's jQuery extensions */
+jQueryExt = { version: 1.1 }
+console.debug("Loading jQueryExt v"+jQueryExt.version);
+
+
 /* returns all attributes of a node */
 $.fn.getAllAttributes = function() {var attributes = {};
 if( this.length ) {$.each( this[0].attributes, function( index, attr ) {attributes[ attr.name ] = attr.value;
@@ -11,4 +16,8 @@ $.fn.asobject=function(){var c={};
 var b=this.serializeArray();
 $.each(b,function(){if(c[this.name]!==undefined){if(!c[this.name].push){c[this.name]=[c[this.name]]}c[this.name].push(this.value||"")}else{c[this.name]=this.value||""}});
 return c};
+
+/* disable/enable form elements */
+$.fn.disable=function(){$(this).attr("disabled",true);};
+$.fn.enable=function(){$(this).removeAttr("disabled");};
 
