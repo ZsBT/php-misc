f3b4fd961be9c8ec696a4326cb9f47dae21ec03e
ZsBT <zsombibi@gmail.com>
2014-02-15 15:35:20 +0100

    adding comments from public functions

--- zsdb3.php
+++ zsdb3.php
@@ -1,25 +1,8 @@
 <?php /*
-	20102-2013 (c) [Z]sombor's [S]imple [D]ata[B]ase	v3.0
+	2012-2013 (c) [Z]sombor's [S]imple [D]ata[B]ase	v3.0
 	
-	usage:
-	
-	include "zsdb3.php";
-	
-	$db = new zsdb3($connspec);
-	
+	See README.md in directory 'zsdb3' for usage
 
-	for psql, mysql, mysqli, mssql, oracle, connspec syntax is :
-	
-	type::dbname@host[:port][/user:password]
-	
-	eg: mysqli::mydb@localhost/username:s3cr3tp@ss
-	
-
-	for sqlite, connspec is:
-	
-	sqlite::/path/to/sqlite3.db
-	
-	
 */
 
 
@@ -71,14 +54,20 @@ class zsdb3 {
     return $D;
   }
   
-  function sql_escape($data) {
+  private function sql_escape($data) {
       return sprintf("'%s'", str_replace("'","`",$data) );
       if(is_numeric($data))return $data;
       $unpacked = unpack('H*hex', $data);
       return '0x' . $unpacked['hex'];
   }
   
-  function insert($table, $datarr) {
+  private function fatal($msg){die("\n\tZSDB3: Fatal error: $msg\n\n");}
+  
+  function __call($method, $args){	// for a function in specific class
+    return call_user_func_array(array($this->D,$method), $args);
+  }
+  
+  public function insert($table, $datarr) {	/* inserts an array-specified row to a table */
     if(!$table)return false;
     if(!$datarr)return false ;
     foreach($datarr as $k=>$v){
@@ -88,9 +77,10 @@ class zsdb3 {
     $Q = sprintf("insert into $table (%s) values (%s)", implode(',',$ka), implode(',',$va) );
 	return $this->exec($Q);
   }
-  function i($table,$datarr){return $this->insert($table,$datarr);}
+  public function i($table,$datarr)	/* alias for insert() */
+    {return $this->insert($table,$datarr);}
 
-  function update($table, $datarr, $cond=0 ) {
+  public function update($table, $datarr, $cond="" ) { /* updates a row */
     if(!$cond)return false;	// uncomment if brave
     if(!$table)return false;
     if(!$datarr)return false;
@@ -99,9 +89,10 @@ class zsdb3 {
     if($cond)$Q.=" where $cond";
     return $this->exec($Q);
   }
-  function u($table,$datarr,$cond=0){return $this->update($table,$datarr,$cond);}
+  public function u($table,$datarr,$cond=0) /* alias for update() */
+    {return $this->update($table,$datarr,$cond);}
   
-  function iou($t, $datarr, $cond=0) {	/* delete first */
+  public function iou($t, $datarr, $cond=0) {	/* deletes then inserts a row */
     if(!$cond)return false;
     $this->exec("delete from $t where $cond");
     return $this->insert($t,$datarr);
@@ -109,31 +100,27 @@ class zsdb3 {
 
 
 
-  function Q($query) { /* gives a simple value; use for a query that returns one row and one field */
+  public function Q($query) { /* Query one field of one row. returns a simple value */
     $R=$this->query($query);
     $fa=$this->fan($R);
     if($R)$this->free($R);else return false;
     return $fa[0];
   }
-  function QFO($query) { /* object of the first row */ $R=$this->query($query);$fo=$this->fo($R);if ($R) $this->free($R);return $fo;}
-  function QFA($query) { /* array of the first row */ $R=$this->query($query);$fa=$this->faa($R);if ($R) $this->free($R);return $fa;}
-  function QA($query) { /* array of a single column from multiple records */ $R=$this->query($query);$ret=array();while ($fa=$this->fan($R)) $ret[]=$fa[0];if ($R) $this->free($R);return $ret;}
-  function QAA($query) { /* gives an array of associative arrays; use for small amount of rows */
-    $R=$this->query($query);$ret=array();while ($fa=$this->faa($R)) $ret[]=$fa;if ($R) $this->free($R);
-    return $ret;
-  }
-  function QOA($query) { /* gives an array of objects; use for small amount of rows */
-    $R=$this->query($query);$ret=array();while ($fo=$this->fo($R)) $ret[]=$fo;if ($R) $this->free($R);
-    return $ret;
-  }
-  function btrans(){ return $this->in_transaction=$this->exec("begin transaction"); }
-  function commit(){ $this->in_transaction=false; return $this->exec("commit");}
-  function rollback(){ $this->in_transaction=false; return $this->exec("rollback");}
-  
-  private function fatal($msg){die("\n\tZSDB3: Fatal error: $msg\n\n");}
-  
-  public function __call($method, $args){	// for a function in specific class
-    return call_user_func_array(array($this->D,$method), $args);
-  }
+  public function QFO($query) /* queries the first row then returns as an object */
+    { $R=$this->query($query);$fo=$this->fo($R);if ($R) $this->free($R);return $fo;}
+  public function QFA($query) /* queries the first row then returns as an array */
+    { $R=$this->query($query);$fa=$this->faa($R);if ($R) $this->free($R);return $fa;}
+  public function QA($query) /* queries one field of several rows */
+    { $R=$this->query($query);$ret=array();while ($fa=$this->fan($R)) $ret[]=$fa[0];if ($R) $this->free($R);return $ret;}
+  public function QAA($query) /* queries array of rows, elements are associated array of fields */
+    { $R=$this->query($query);$ret=array();while($fa=$this->faa($R))$ret[]=$fa;if($R)$this->free($R);return $ret; }
+  public function QOA($query) /* queries array of rows, elements are objects of fields */
+    { $R=$this->query($query);$ret=array();while ($fo=$this->fo($R)) $ret[]=$fo;if($R)$this->free($R);return $ret;}
+  public function btrans() /* begin transaction */
+    { return $this->in_transaction=$this->exec("begin transaction"); }
+  public function commit() /* commit transaction */
+    { $this->in_transaction=false; return $this->exec("commit");}
+  public function rollback() /* rollback transaction */
+    { $this->in_transaction=false; return $this->exec("rollback");}
   
 }
