9f719820d81c6382f97df0b0c73ba075ce2eda40
ZsBT <kalo@zsombor.net>
2015-03-11 09:27:45 +0100

    added oneCol() and iterate()

--- misc/zsPDO.class.php
+++ misc/zsPDO.class.php
@@ -8,15 +8,24 @@
 class zsPDO extends PDO {
 
     public function oneValue($sql){  /* returns the first column of the first row of the query */
-        $st = $this->prepare($sql);
+        if(!$st = $this->prepare($sql))
+            throw new Exception("Prepare statement error: ".json_encode($this->errorInfo()) );
         $st->execute();
         return $st->fetch(PDO::FETCH_NUM)[0];
     }
     
-    public function oneRow($sql){  /* returns the first row of a statement, as stdClass object  */
-        $st = $this->prepare($sql);
+    public function oneCol($sql){  /* returns the first column of all rows of the query */
+        if(!$st = $this->prepare($sql))
+            throw new Exception("Prepare statement error: ".json_encode($this->errorInfo()) );
         $st->execute();
-        return $st->fetchObject();
+        return $st->fetchAll(PDO::FETCH_COLUMN,0);
+    }
+    
+    public function oneRow($sql,$mode=PDO::FETCH_OBJ){  /* returns the first row of a statement, as stdClass object  */
+        if(!$st = $this->prepare($sql))
+            throw new Exception("Prepare statement error: ".json_encode($this->errorInfo()) );
+        $st->execute();
+        return $st->fetch($mode);
     }
     
     public function allRow($sql,$mode=PDO::FETCH_CLASS){  /* returns an array of stdClass objects - be sure to use only reasonable number of records. */
@@ -26,6 +35,16 @@ class zsPDO extends PDO {
         $fa = $st->fetchAll($mode);
         return $fa;
     }
+    
+
+    public function iterate($sql, $function,$mode=PDO::FETCH_CLASS){	/* pass every record object as parameter to $function  */
+        if(!$st = $this->prepare($sql))
+            throw new Exception("Prepare statement error: ".json_encode($this->errorInfo()) );
+        $st->execute();
+        while($fo = $st->fetch($mode))$function($fo);
+        return true;
+    }
+    
     
     public function insert($table, $datArr){  /* insert data to a table. datArr is a mapped array. no BLOB support */
         $keys = @array_keys($datArr);
@@ -40,6 +59,7 @@ class zsPDO extends PDO {
         if(!$st->execute())return false;
         return ($ID=$this->lastInsertId())? $ID:true ;
     }
+
     public function update($table, $datArr, $cond){  /* update data in a table. datArr is a mapped array. $cond is the condition string */
         $keys = @array_keys($datArr);
         
