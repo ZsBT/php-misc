57f772c3727e4ac03fba66f39bf4b63a17d8f474
ZsBT <zsombor@fociologia.hu>
2015-10-23 12:45:56 +0200

    remove expired files

--- misc/zsCache.php
+++ misc/zsCache.php
@@ -1,20 +1,37 @@
 <?php /*
 
-	(c) Zsombor simple cache class with methods set and get				*/
+	(c) Zsombor simple cache class with methods set and get				
+	
+	
+CHANGELOG
+  2015-10	remove expired files
+  	
+	*/
 
-#	uses the temporary filesystem	
 
 class zsCache {
+  
   private function fnbykey($key){return $this->PF.md5($key);}
+
   function __construct($timeout=360,$prefix=''){
     $this->TO=$timeout;
     $this->PF=sys_get_temp_dir()."/.zscache_{$prefix}_";
   }
-  function set($key,$data){ return file_put_contents($this->fnbykey($key), serialize($data));}
-  function get($key){
+
+  
+  public function set($key,$data){
+    return file_put_contents($this->fnbykey($key), serialize($data));
+  }
+
+  
+  public function get($key){
     $fn=$this->fnbykey($key);
     if(!file_exists($fn))return NULL;
-    if(time()-filemtime($fn)>$this->TO)return NULL;
+    if(time() - filemtime($fn)>$this->TO){
+      unlink($fn);
+      return NULL;
+    }
     return unserialize(file_get_contents($fn));
   }
+
 }
