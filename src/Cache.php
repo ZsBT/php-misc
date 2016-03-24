fac4c36662a9389678abad4f83374d96ee792ef9
ZsBT <kalo@zsombor.net>
2016-03-24 11:53:07 +0100

    name case fix

---
+++ src/Cache.php
@@ -0,0 +1,39 @@
+<?php /*
+
+	(c) Zsombor simple cache class with methods set and get				
+	
+	
+CHANGELOG
+  2015-10	remove expired files
+  	
+	*/
+
+namespace ZsBT\misc;
+
+class Cache {
+  
+  private function fnbykey($key){return $this->PF.md5($key);}
+
+  function __construct($timeout=360,$prefix=''){
+    $this->TO=$timeout;
+    $this->PF=sys_get_temp_dir()."/.zscache_{$prefix}_";
+  }
+
+  
+  public function set($key,$data){
+    return file_put_contents($this->fnbykey($key), serialize($data));
+  }
+
+  
+  public function get($key){
+    $fn=$this->fnbykey($key);
+    if(!file_exists($fn))return NULL;
+    if(time() - filemtime($fn)>$this->TO){
+      unlink($fn);
+      return NULL;
+    }
+    return unserialize(file_get_contents($fn));
+  }
+
+}
+?>
